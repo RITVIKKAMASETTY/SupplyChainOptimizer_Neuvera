3df4170512587b9371cfbe7047807df57003f776
VaibhavK <115970059+TusharVaibhavK@users.noreply.github.com>
2025-04-19 10:51:42 +0530

    Deleted app_test.py

--- app_test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Streamlit Test App")
-st.write("If you can see this, Streamlit is working correctly!")
-
-slider_value = st.slider("Test slider", 0, 100, 50)
-st.write(f"Selected value: {slider_value}")
